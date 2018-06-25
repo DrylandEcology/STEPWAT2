DrylandEcology/STEPWAT2

commit 9c817c6c9109433f27a2c10e9a10c201b90f988b

Finished function to calculate dSWAbulk

ST_resgroups.c
@@ -101,8 +101,8 @@ void rgroup_PartResources(void)
 	RealF resource, /* amt of "resource" == 1 when ppt is avg */
 	xtra_base = 0., /* pooled extra resource up to 1.0 */
 	xtra_obase = 0., /* pooled resource > 1.0 */
-	size_base[MAX_RGROUPS], /* total res. contrib to base, all groups */
-	size_obase[MAX_RGROUPS]; /* total res. contrib. if xtra_obase */
+	size_base[MAX_RGROUPS] = {0}, /* total res. contrib to base, all groups */
+	size_obase[MAX_RGROUPS] = {0}; /* total res. contrib. if xtra_obase */
 
 	Bool noplants = TRUE;
 	const Bool do_base = FALSE, /* monikers for _res_part_extra() */
@@ -913,7 +913,7 @@ void RGroup_Update_Newsize(GrpIndex rg)
 	indivs = RGroup_GetIndivs(rg, SORT_0, &numindvs);
 	for (n = 0; n < numindvs; n++)
 		indivs[n]->grp_res_prop = indivs[n]->relsize / sumsize;
-	Mem_Free(indivs);
+	//Mem_Free(indivs); // dont call free on variable unless it was initialized with malloc or calloc
 	///}
 
 	/* double check some assumptions */

commit 9a9520970dba333970979a3d07b583bc3fb9fcc6

Fix memory-deallocation in function `RGroup_GetIndivs`
- address #147

- commit https://github.com/DrylandEcology/STEPWAT2/commit/9c817c6c9109433f27a2c10e9a10c201b90f988b mistakenly commented `Mem_Free(indivs);` out and ironically added as comment the exact reason, i.e., "dont call free on variable unless it was initialized with malloc or calloc", why this call to free must not be commented out ...!

ST_resgroups.c
@@ -777,7 +777,8 @@ void RGroup_Update_Newsize(GrpIndex rg)
 	indivs = RGroup_GetIndivs(rg, SORT_0, &numindvs);
 	for (n = 0; n < numindvs; n++)
 		indivs[n]->grp_res_prop = indivs[n]->relsize / sumsize;
-	//Mem_Free(indivs); // dont call free on variable unless it was initialized with malloc or calloc
+
+	Mem_Free(indivs);
 
 	/* double check some assumptions */
 	if (RGroup[rg]->est_count < 0)