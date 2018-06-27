f1bd414d691c06019a2c6c78b75069a2820816c0
kpalmqui <kpalmqui>
2018-06-27 10:40:04 -0600

    Final commit to implement #20
    - updated to 4.4C (mainly because we have a citation for this)
    - updated comment to reflect testing required and moving of this parameter to env.in

--- sxw.h
+++ sxw.h
@@ -28,7 +28,9 @@
 //  * growing season at CPER is 1 April - 30 September
 //  * growing season ppt = 0.86 * annual ppt at CPER
 // A base of 5 C will on average include April - October for Fort Collins recent climate
-#define GROWING_BASE_TEMP 5. // base-temperature in degree Celsius
+// A base of 4.4 C as described in Sims et al. 1978 Journal of Ecology
+// Additional testing required here and GROWING_BASE_TEMP should eventually be moved to env.in
+#define GROWING_BASE_TEMP 4.4 // base-temperature in degree Celsius
 
 
 #include "generic.h"
